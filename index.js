6e3db7bca0f7fdd9414de3543eb88b62e9516ac7
Lance Miller <lmiller@bamboohr.com>
2018-11-18 23:20:52 -0700

    more api-friendly structure

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function cleanRecord(obj, prefix) {
 }
 
 function sortKeys(obj) {
+	if (typeof obj === 'string') {
+		return obj;
+	}
+
 	if (typeof (obj.id) !== 'undefined') {
 		delete obj.id;
 	}
@@ -72,32 +76,25 @@ function select(table, where = null) {
 		.map((val) => cleanRecord(val, table.replace(/s$/, '')));
 }
 
-const index = {
-	volumes: [],
-	books: [],
-};
+const index = {};
 const rootDir = path.join(__dirname, 'json');
 
 select('volumes').forEach((volume) => {
-	index.volumes.push(volume.name);
-
 	volume.books = [];
 	volume.bookCount = 0;
 	volume.chapterCount = 0;
 	volume.verseCount = 0;
 
-	const volDir = path.join(rootDir, volume.name);
-
 	select('books', { volume_id: volume.id }).forEach((book) => {
-		index.books.push(book.name);
-
 		volume.books.push(book.name);
 		volume.bookCount++;
+
+		delete book.volumeId;
 
 		book.chapterCount = 0;
 		book.verseCount = 0;
 
-		const bookDir = path.join(volDir, book.name);
+		const bookDir = path.join(rootDir, book.name);
 
 		select('chapters', { book_id: book.id }).forEach((chapter) => {
 			volume.chapterCount++;
@@ -107,33 +104,29 @@ select('volumes').forEach((volume) => {
 
 			chapter.verseCount = 0;
 
-			const chDir = path.join(bookDir, String(chapter.number));
-
-			const verses = select('verses', { chapter_id: chapter.id })
+			chapter.verses = select('verses', { chapter_id: chapter.id })
 				.map((verse) => {
 					delete verse.chapterId;
-					verse.chapter = chapter.number;
 
-					volume.verseCount++;
-					book.verseCount++;
-					chapter.verseCount++;
-
-					return verse;
+					return sortKeys(verse);
 				});
+			
+			chapter.verseCount = chapter.verses.length;
+			volume.verseCount += chapter.verseCount;
+			book.verseCount += chapter.verseCount;
 
-			makeFile([chDir, 'index.json'], chapter);
-			makeFile([chDir, 'verses.json'], verses);
+
+			makeFile([bookDir, `${ chapter.number }.json`], chapter);
 		});
 
-		makeFile([bookDir, 'index.json'], book);
-		// console.log(book);
+		makeFile([rootDir, `${ book.name }.json`], book);
+		console.log(book);
 	});
 
-	makeFile([volDir, 'index.json'], volume);
-	console.log(volume);
+	index[volume.name] = volume.books;
 });
 
 makeFile([rootDir, 'index.json'], index);
-// console.log(index);
+console.log(index);
 
 console.log('DONE!');
